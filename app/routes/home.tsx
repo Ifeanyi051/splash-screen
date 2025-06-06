7c803c97764f7a1c866c1eec6f6292d9c9cd955a
Ifeanyi051 <150017710+Ifeanyi051@users.noreply.github.com>
2025-06-06 22:03:46 +0100

    push

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Purple splash screen" },
+    { name: "description", content: "created by codelikeifeanyi.com" },
   ];
 }
 
