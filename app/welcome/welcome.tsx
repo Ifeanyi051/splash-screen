3177e14a6071681835b29a10525a5ea43959ad94
Ifeanyi051 <150017710+Ifeanyi051@users.noreply.github.com>
2025-06-09 11:38:56 +0100

    checking

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -26,41 +26,72 @@ import { gsap } from 'gsap';
 export function Welcome() {
   const windowOne = useRef(null);
   const windowTwo = useRef(null);
+  const windowWelcome = useRef(null);
 
   useEffect(() => {
+
+    gsap.to(windowOne.current, {
+      rotate: 29,
+      duration: 1,
+      delay: .3
+    });
+
+    gsap.to(windowTwo.current, {
+      rotate: 29,
+      duration: 1,
+      delay: .3
+    });
+
+
+
     gsap.to(windowOne.current, {
       duration: 1,
       x: -500,
+      delay: 1
     });
 
     gsap.to(windowTwo.current, {
       duration: 1,
       x: 500,
+      delay: 1
+    });
+
+    gsap.to(windowWelcome.current, {
+      duration: 1,
+      scale: 1,
+      opacity: 1,
+      delay: 1
     });
 
 
   }, []);
 
   return (
-    <main className="overflow-hidden h-screen w-screen relative">
+    <main className="overflow-hidden h-screen w-screen relative bg-[white]">
       <div className="overflow-hidden h-full w-full relative">
         <img
           src="Rectangle33.svg"
           alt=""
-          className="h-[150vh] w-[150vw] absolute left-[-45vw] top-0 z-[2] rotate-[25deg] scale-125"
+          className="h-[120vh] min-w-[150vw] absolute left-[-45vw] top-[-60px] z-[2] scale-125"
           ref={windowOne}
         />
         <img
           src="Rectangle34.svg"
           alt=""
-          className="h-[150vh] w-[150vw] absolute right-[-55vw] top-0 rotate-[25deg] translate-x-[-150px] scale-125"
+          className="h-[120vh] min-w-[150vw] absolute right-[-55vw] top-[-60px] scale-125"
           ref={windowTwo}
         />
+        <div className='flex justify-center items-center h-full w-full'>
+          <h1 ref={windowWelcome} className='text-[36px] font-bold text-[#884DD4] scale-50 opacity-0'>Welcome</h1>
+          <div className='bg-[red] min-h-[300px] min-w-[300px] absolute opacity-25 rounded-full scale-500'>
+            
+          </div>
+        </div>
+
+        
       </div>
 
-      <div>
-        <h1>Welcome</h1>
-      </div>
+      
     </main>
   );
 }
